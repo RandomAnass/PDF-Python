e0e34e62c9d5c2596d042290c7f3f7f1f207a1e3
Anass AL AMMIRI <51008418+RandomAnass@users.noreply.github.com>
2021-08-21 05:30:15 +0200

    Create Hough_transform.py

---
+++ Hough_transform.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 21 05:15:14 2021
+
+@author: anass
+"""
+
+import cv2
+import numpy as np
+
+img = cv2.imread(r"C:\Users\anass\Programmation\PDF\p.jpg")
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+minLineLength = 100
+maxLineGap = 10
+lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+print(lines)
+for x1,y1,x2,y2 in lines[0]:
+    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+
+cv2.imwrite('houghlines5.jpg',img)
